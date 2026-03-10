24a6efb313fb887b62e953059ef2a0136d16532b
marco.girardi <marco.girardi@factorymind.com>
2026-03-10 18:03:40 +0100

    sortolino

--- Pokedex.Angular/src/app/list/list.ts
+++ Pokedex.Angular/src/app/list/list.ts
@@ -1,7 +1,7 @@
 import { Component, input } from '@angular/core';
 import { MockData } from '../../Types/mockData';
 import { DataService } from '../../Services/dataService';
-import { ListItem } from "./list-item/list-item";
+import { ListItem } from './list-item/list-item';
 
 @Component({
   selector: 'app-list',
@@ -10,9 +10,20 @@ import { ListItem } from "./list-item/list-item";
   styleUrl: './list.css',
 })
 export class List {
-
   // Since we get here directly from the root of the app
   // (through a routerOutlet), we just hardcode the data
-  public data : MockData[] = DataService.data;
+  public data: MockData[] = DataService.data;
+  public originalData = [...this.data];
+
+  protected sortByAddress() {
+    // ooh very brutto, mutates in place?
+    this.data.sort((a, b) => a.address.toString().localeCompare(b.address.toString()));
+  }
+  protected sortByName() {
+    this.data.sort((a, b) => a.name.localeCompare(b.name));
+  }
+  protected sortByNumber() {
+    this.data.sort((a, b) => a.number - b.number);
+  }
 
 }
