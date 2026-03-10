44811fed2d8fd1b386abdc228dba8236d5a44588
marco.girardi <marco.girardi@factorymind.com>
2026-03-10 13:37:43 +0100

    Bold of you to assume I know what I am doing

---
+++ Pokedex.Angular/src/Services/dataService.ts
@@ -0,0 +1,30 @@
+// Ideally I'll put here a way to access to the data I want to display.
+// For now I do it in a stupid way.
+// Ideally in the future I may want to do it... better? idk
+// I wonder if one can avoid a class
+
+import { MockData } from "../Types/mockData";
+
+export class DataService {
+    public data : MockData[] = [
+        {
+            number : 4,
+            name : 'gigi pethot',
+            address : {
+                city: 'Garganello',
+                road: 'via dei fanciulli',
+                number: 4
+            }
+        },
+        {
+            number : 6,
+            name : 'andrew ronk',
+            address : {
+                city: 'Varnese',
+                road: 'via dal progetto',
+                number: 44
+            }
+        }
+    ]
+
+}
---
+++ Pokedex.Angular/src/Types/mockData.ts
@@ -0,0 +1,11 @@
+export interface MockData {
+    number : number,
+    name : string,
+    address : Address
+}
+
+export interface Address{
+    road: string,
+    city : string,
+    number : number
+}
